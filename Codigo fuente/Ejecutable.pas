2707fe4c90d9bf1cfbca2e79b5b4b3ff6922189f
tomas <tomas.b.h@ciencias.unam.mx>
2024-11-10 00:16:50 -0600

    Corregí el Ejecutable, donde se le puede pasar la ruta de la imagen y ya calcula el cci

--- Codigo fuente/Ejecutable.pas
+++ Codigo fuente/Ejecutable.pas
@@ -1,16 +1,29 @@
 program Ejecutable;
 
 uses
-SysUtils, Classes, Programa;
+  SysUtils, Classes, Programa;
 
-function Ejecutable(const NombreArchivo: String);
+procedure Ejecutable(const NombreArchivo: String);
 var
-    Pixeles: TArregloPixeles;
-    Radio: Integer;
-    Indice: Real;
+  Pixeles: TArregloPixeles;
+  Radio: Integer;
+  Indice: Real;
 begin
-    Pixeles := LectorPixeles(NombreArchivo);
-    Radio := CalcularRadioCirculo(Pixeles);
-    ConvertirBlancoYNegro(Pixeles, Radio);
-    GuardarImagen(Pixeles, 'Resultado.jpg');
-    Indice := CalcularIndice(Pixeles);
+  Pixeles := LectorPixeles(NombreArchivo);
+  Radio := CalcularRadioCirculo(Pixeles);
+  ConvertirBlancoYNegro(Pixeles, Radio);
+  GuardarImagenRecortada(Pixeles, 'Resultado.jpg');
+  Indice := CalcularIndice(Pixeles, Radio);
+  WriteLn('Índice de cobertura nubosa: ', Indice:0:2, '%');
+end;
+
+var
+  RutaImagen: String;
+begin
+  Write('Ingrese la ruta de la imagen: ');
+  ReadLn(RutaImagen);
+  if FileExists(RutaImagen) then
+    Ejecutable(RutaImagen)
+  else
+    WriteLn('Error: No se encontró el archivo ', RutaImagen);
+end.
